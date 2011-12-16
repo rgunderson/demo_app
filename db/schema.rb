5a27976ebd207fb2095eb5c0392d1934ebfe7838
J.R. Gunderson <ralph@rgunderson.com>
2011-12-15 21:28:00 -0500

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111215213900) do
-
-  create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20111215203230) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
